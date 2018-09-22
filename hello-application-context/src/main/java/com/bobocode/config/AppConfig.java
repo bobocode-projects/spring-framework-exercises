3b1aa79d46fcc605d82e404586ff23a6445acd0b
tboychuk <tarass.boychuk@gmail.com>
2018-09-22 22:02:45 +0300

    Update todo section in the class AppConfig.java

--- hello-application-context/src/main/java/com/bobocode/config/AppConfig.java
+++ hello-application-context/src/main/java/com/bobocode/config/AppConfig.java
@@ -7,7 +7,8 @@ import com.bobocode.TestDataGenerator;
  * <p>
  * todo: make this class a Spring configuration class
  * todo: enable component scanning for dao and service packages
- * todo: configure a bean of type {@link TestDataGenerator} with name "dataGenerator". Don't specify bean name explicitly
+ * todo: provide explicit configuration for a bean of type {@link TestDataGenerator} with name "dataGenerator" in this class.
+ * todo: Don't specify bean name "dataGenerator" explicitly
  */
 public class AppConfig {
 
