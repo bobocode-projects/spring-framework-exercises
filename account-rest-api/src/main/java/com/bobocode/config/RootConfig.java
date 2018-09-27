f49df5bda0b6dbe9512233a6640f8fea083dcd8a
tboychuk <tarass.boychuk@gmail.com>
2018-09-27 17:49:25 +0300

    Complete the exercise "Account REST API"

--- account-rest-api/src/main/java/com/bobocode/config/RootConfig.java
+++ account-rest-api/src/main/java/com/bobocode/config/RootConfig.java
@@ -1,5 +1,9 @@
 package com.bobocode.config;
 
+import com.bobocode.TestDataGenerator;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,5 +14,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * todo: 2. Enable component scanning for all packages in "com.bobocode" using annotation property "basePackages"
  * todo: 3. Exclude web related config and beans (ignore @{@link Controller}, ignore {@link EnableWebMvc})
  */
+@Configuration
+@ComponentScan(basePackages = "com.bobocode", excludeFilters = {@Filter(EnableWebMvc.class), @Filter(Controller.class)})
 public class RootConfig {
 }
+
