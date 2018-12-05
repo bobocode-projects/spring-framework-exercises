2d97d1b6d4db5cc23ee4147224f78d0347a6bf33
shtramak <shtramak@gmail.com>
2018-12-05 17:00:11 +0200

    implement hello-spring-mvc

--- hello-spring-mvc/src/main/java/com/bobocode/config/RootConfig.java
+++ hello-spring-mvc/src/main/java/com/bobocode/config/RootConfig.java
@@ -1,5 +1,9 @@
 package com.bobocode.config;
 
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -10,5 +14,10 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * todo: enable component scanning for all packages in "com.bobocode"
  * todo: ignore all web related config and beans (ignore @{@link Controller}, ignore {@link EnableWebMvc}) using exclude filter
  */
+@Configuration
+@ComponentScan(
+        basePackages = "com.bobocode",
+        excludeFilters = {@Filter(type = FilterType.ANNOTATION, value = Controller.class),
+                @Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class)})
 public class RootConfig {
 }
