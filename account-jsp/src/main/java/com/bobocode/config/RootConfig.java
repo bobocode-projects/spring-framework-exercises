ddd994dd73b781a9e7931163482aa9eeb1719504
shtramak <shtramak@gmail.com>
2018-12-09 17:26:02 +0200

    implement account-jsp

--- account-jsp/src/main/java/com/bobocode/config/RootConfig.java
+++ account-jsp/src/main/java/com/bobocode/config/RootConfig.java
@@ -1,6 +1,11 @@
 package com.bobocode.config;
 
 import com.bobocode.TestDataGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ComponentScan.Filter;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
@@ -12,5 +17,12 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
  * todo: 3. Exclude web related config and beans (ignore @{@link Controller}, ignore {@link EnableWebMvc})
  * todo: 4. Configure {@link TestDataGenerator} bean with name "dataGenerator" (don't specify name explicitly)
  */
+@Configuration
+@ComponentScan(basePackages = "com.bobocode", excludeFilters = {@Filter(type = FilterType.ANNOTATION, value = EnableWebMvc.class),
+        @Filter(type = FilterType.ANNOTATION, value = Controller.class)})
 public class RootConfig {
+    @Bean
+    public TestDataGenerator dataGenerator() {
+        return new TestDataGenerator();
+    }
 }
--- account-jsp/src/main/java/com/bobocode/web/controller/AccountController.java
+++ account-jsp/src/main/java/com/bobocode/web/controller/AccountController.java
@@ -1,7 +1,17 @@
 package com.bobocode.web.controller;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 import com.bobocode.TestDataGenerator;
 import com.bobocode.model.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * This controller provides endpoint that generates a list of {@link Account} and passes it to the view.
@@ -13,6 +23,22 @@ import com.bobocode.model.Account;
  * todo: 5. Provide a default value "10" for parameter "size"
  * todo: 6. Pass the list of accounts to the view using model attribute with name "accountList"
  */
+@Controller
+@RequestMapping(value = "/accounts")
 public class AccountController {
+    private final TestDataGenerator testDataGenerator;
 
+    @Autowired
+    public AccountController(TestDataGenerator testDataGenerator) {
+        this.testDataGenerator = testDataGenerator;
+    }
+
+    @GetMapping
+    public String accountSize(@RequestParam(value = "size", defaultValue = "10") int size, Model model) {
+        List<Account> accounts = Stream.generate(testDataGenerator::generateAccount)
+                .limit(size)
+                .collect(Collectors.toList());
+        model.addAttribute("accountList", accounts);
+        return "accounts";
+    }
 }
